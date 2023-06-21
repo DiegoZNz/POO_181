a2fb9e44eb7f448fb51c524c98fa40c7810e0697
DiegoZNz <121038369@upq.edu.mx>
2023-06-21 17:08:31 -0600

    Practica 6: redirect, inserts DB, flash msg view

--- P2/IntroFlask/app.py
+++ P2/IntroFlask/app.py
@@ -1,6 +1,7 @@
 #aquí se importa el framework 
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
+from flask import flash
 #inicialización del APP
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -8,6 +9,8 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'db_flask'
 mysql = MySQL(app)
+app.secret_key = "mi_clave_secreta"
+
 
 #declaración de la ruta http://localhost:5000
 @app.route('/')#ruta index o raiz, busca el método index y regresa un hola mundo.
@@ -29,13 +32,19 @@ def index():
 @app.route('/guardar', methods=['POST'])
 def guardar():
     if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        año = request.form['txtAnio']
+        #pasamos a variables al contenido de los inputs
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vaño = request.form['txtAnio']
         
-        print(titulo,artista,año)
-        
-    return "los datos llegaron"
+        #haremos la conex a la db y ejecutar el insert
+        CS = mysql.connection.cursor()
+        CS.execute('INSERT INTO tbalbums (Titulo, Artista, Anio) values (%s,%s,%s)',(Vtitulo,Vartista,Vaño))
+        mysql.connection.commit()
+    flash('El album fue agregado correctamente')
+    return redirect(url_for('index'))
+
+
 
 
 @app.route('/eliminar')
